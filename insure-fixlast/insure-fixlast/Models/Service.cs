02610eeb4ad16984ac4ad989e29054cbb69e90df
king-berry <111548775+king-berry@users.noreply.github.com>
2024-05-22 23:18:05 +0700

    fix db

---
+++ insure-fixlast/insure-fixlast/Models/Service.cs
@@ -0,0 +1,17 @@
+﻿namespace insure_fixlast.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string benefit { get; set; }
+        public decimal price { get; set; }
+        public Rank rank { get; set; }
+        public int Quantity { get; set; }
+    }
+    public enum Rank
+    {
+        R, SR, SSR, UR, LR
+    }
+}
